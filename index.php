a1d40f389235eff33d9f817ece62123ca845d54d
unknown <2120367@talnet.nl>
2020-02-06 10:29:09 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+
+if(!isset($_COOKIE['loggedInUser']))
+{
+header("Location: login.php");
+}
+
+$localhost = 'localhost';
+$db = 'netland';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$localhost;dbname=$db;charset=$charset";
+
+try 
+{
+    $pdo = new PDO($dsn, $user, $pass);
+} 
+catch (\PDOException $e)
+{
+    echo 'error connecting to database :( on line : ' . $e->getMessage();
+}
+
+
+
+$sql = "SELECT * FROM media";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+
+$media = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+function displaySeries($key)
+{
+    echo 
+    '<tr>' .
+        '<td>' . $key->title . '</td>' .
+        '<td>' . $key->rating . '</td>' .
+        '<td>' . "<a href='details.php?id=$key->id'>details</a>" . '</td>' .
+    '</tr>';
+}
+
+function displayFilms($key)
+{
+    echo 
+    '<tr>' .
+        '<td>' . $key->title . '</td>' .
+        '<td>' . $key->duration . '</td>' .
+        '<td>' . "<a href='details.php?id=$key->id'>details</a>" . '</td>' .
+    '</tr>';
+}
+
+
+
+function display($type,$callback)
+{
+    global $media;
+    foreach ($media as $key) 
+    {
+        if($key->type == $type)
+        {
+            $callback($key);
+        }
+    }
+}
+
+
+
+
+
+?>
+<table>
+<h3>Series</h3>
+<tr>
+<th>Title</th>
+<th>Rating</th>
+</tr>
+<tr>
+<?php display('serie', 'displaySeries'); ?>
+</tr>
+</table>
+
+<br>
+
+<form action="create.php" method="get">
+<input type="hidden" name="type" value="serie">
+<input type="submit" name="submit" value="Add serie">
+</form>
+
+<br>
+
+<table>
+<h3>Films</h3>
+<tr>
+<th>Title</th>
+<th>Duur</th>
+</tr>
+<tr>
+<?php display('film', 'displayFilms'); ?>
+</tr>
+</table>
+
+<br>
+
+<form action="create.php" method="get">
+<input type="hidden" name="type" value="film">
+<input type="submit" name="submit" value="Add film">
+</form>
+
+
+
+
---
+++ login.php
@@ -0,0 +1,24 @@
+<?php
+
+function getError()
+{
+    if(isset($_GET['error']))
+    {
+        return 'Incorrect Username or Password';
+    }
+
+    return '';    
+}
+
+?>
+
+<h2>Netland Admin Panel</h2>
+<form action="process_login.php" method="post">
+<label for="username">Username</label>
+<input type="text" name="username" placeholder="Username">
+<br></br>
+<label for="password">Password</label>
+<input type="password" name="password" placeholder="Password">
+<h3 style="color: red;"><?php echo getError(); ?></h3>
+<input type="submit" name="login">
+</form>
